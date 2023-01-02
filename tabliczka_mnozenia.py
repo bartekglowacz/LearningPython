90a252d4d435e7c2edcf31ee05d817a14ab6e63f
bartekglowacz <bartekglowacz@gmail.com>
2023-01-02 18:47:50 +0100

    tabliczka mnożenia

--- tabliczka_mnozenia.py
+++ tabliczka_mnozenia.py
@@ -1,10 +1,9 @@
-print("Wyświetla tabliczkę mnożenia: ")
+print("Wyświetla tabliczkę mnożenia: \nDla jakiej cyfry wyświetlić?")
 
-i = 1
+i = int(input())
 j = 1
 while i <= 10:
     while j <= 10:
-        print(i*j, end="\t")
+        print(i, "*", j, "=", i*j)
         j += 1
     i += 1
-
