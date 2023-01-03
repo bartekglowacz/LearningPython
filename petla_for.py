d54934588b03c10a4fb41679a5297b223a11cbaa
bartekglowacz <bartekglowacz@gmail.com>
2023-01-03 15:43:23 +0100

    Dodano wyświetlanie liczb nieparzystych ze zbioru 0-10

--- petla_for.py
+++ petla_for.py
@@ -1,7 +1,18 @@
+"""
 lista = []
 i = 97
 while i < 107:
     lista.append(chr(i))
     i += 1
 print("Utworzona lista: ", lista)
+"""
 
+print("Wpisz swoje imię: ")
+imie_in=input()
+if len(imie_in) == 1:
+    print("Imię składa się z: ", len(imie_in), " litery")
+else:
+    print("Imię składa się z: ", len(imie_in), " liter")
+
+for i in range (0, len(imie_in)):
+    print("Litera nr ", i+1, "to ", imie_in[i])
