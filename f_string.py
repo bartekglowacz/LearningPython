1b4e78ebe5a724cadd415ec40471ee71e1c48548
bartekglowacz <bartekglowacz@gmail.com>
2023-01-12 20:23:14 +0100

    zadanko z f-string

--- f_string.py
+++ f_string.py
@@ -1,10 +1,11 @@
-name = []
-surname = []
+name_surname = []
 
 print("Ile osób chcesz wpisać?")
 number_of_people = int(input())
 print("Wpisz imie i nazwisko dla osoby nr ")
 for x in range (0, number_of_people):
-    name.append(input())
-    surname.append(input())
-    print(f"{x+1} to {name}, {surname}")
+    print(x+1,":")
+    name_surname.append(input())
+print(f"{name_surname}")
+
+
