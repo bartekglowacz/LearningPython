1571cea7d797f85422dd76617f195ba47776f0be
bartekglowacz <bartekglowacz@gmail.com>
2023-01-16 15:01:49 +0100

    ok

--- Give_name_surname.py
+++ Give_name_surname.py
@@ -1,7 +1,30 @@
-import function_name
-import function_surname
+person = "                                                                     "
+print("Podaj imię i nazwisko: ")
+data = input()
+x = len(data) - 1
 
-dane_osobowe = input()
 
-function_name(dane_osobowe)
-function_surname(dane_osobowe)
+def function_name(data, y=len(data)):
+    global person
+    for y in range(0, y, 1):
+        if data[y] == " ":
+            break
+        else:
+            # print(data[x], end="")
+            person = person.lstrip(" ") + data[y]
+    #print(f"Imię: {person}")
+    return person
+
+
+def function_surname(data):
+    for i in range(x, -1, -1):
+        if data[i] == " ":
+            break
+        else:
+            person = data[i::+1]
+    #print(f"Nazwisko: {person}")
+    return person
+
+
+print(f"Imię osoby to: {function_name(data)}")
+print(f"Nazwisko osoby to: {function_surname(data)}")
