588c8f24697b3b234801dad1bce15742c922a6cf
bartekglowacz <bartekglowacz@gmail.com>
2023-01-16 15:48:39 +0100

    nic do zgłoszenia

--- run_module.py
+++ run_module.py
@@ -1,5 +1,6 @@
 #import module # importujemy cały moduł, ze wszystkiki funkcjami w nim
-from module import test, apple # importujemy konkretną funkcję z danego modułu
+#from module import test, apple # importujemy konkretną funkcję z danego modułu
+from module import * #importuje wszsytkie funkcje z modułu (bez konieczności wywoływania ich po kropce)
 
 print("Hello world")
 test("bar")
