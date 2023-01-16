b81266803516e95d4d056fdebe6284ae36ffdbfa
bartekglowacz <bartekglowacz@gmail.com>
2023-01-16 15:02:17 +0100

    nauka modułów

---
+++ module.py
@@ -0,0 +1,7 @@
+def test(text):
+    print("Test: ", end='')
+    print(text)
+
+def apple(text):
+    print("Apple: ", end='')
+    print(text)
---
+++ run_module.py
@@ -0,0 +1,9 @@
+#import module # importujemy cały moduł, ze wszystkiki funkcjami w nim
+from module import test, apple # importujemy konkretną funkcję z danego modułu
+
+print("Hello world")
+test("bar")
+test("wio")
+test("Koniu!")
+
+apple("JABUUUUSZKO")
