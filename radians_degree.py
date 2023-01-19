57b6baf47ec1495aa84d8372efeee80f82dc216c
bartekglowacz <bartekglowacz@gmail.com>
2023-01-19 20:27:35 +0100

    .Zauważyłem problem. Wszystko działa i ładnie liczy, ale tylko jeśli wpisywana pierwsza cyfra jest jednocyfrowa. Czyli np. 1/999 rad na stopnie zamieni, ale już 11/999 rad nie. 9/9999pi rad na stopnie zamieni, ale już 12/873pi rad nie. To samo z dziesiętnymi: 2.0 rad na stopnie zamieni, ale już 11.3 rad nie. 9.67pi rad na stopnie zamieni, ale już 33.124pi rad nie

--- radians_degree.py
+++ radians_degree.py
@@ -8,11 +8,33 @@ You can also import the value of Pi from Python’s math module.
 """
 import math
 
-print("Podaj wartość kąta w radianach")
+print("""Podaj wartość kąta w radianach w któreś z poniższych postaci:\n- 2.0pi\n- 2\n- 1/2pi\n- 1/2 """)
 radian = input()
-radian_left = radian.split(" ")
-print(radian_left)
-radian_right = radian.split(f"{float}")
-print(radian_right)
+radian_partition = radian.partition("pi")
+print(radian_partition, type(radian_partition))
+print(f"Element 0 ma wartość: {radian_partition[0]}, typ: {type(radian_partition[0])}")
+print(f"Element 1 ma wartość: {radian_partition[1]}, typ: {type(radian_partition[1])}")
+# print(float(radian_partition[0]))
 
-#def rad_to_deg(radian):
+#
+
+if radian_partition[0].find(".") == True and radian_partition[1] == ("pi"):
+    print("Ułamek dziesiętny")
+    degrees = float(radian_partition[0]) * 180
+    print(f"{radian} w mierze kątowej jest równe {degrees}")
+if radian_partition[0].find(".") == True and radian_partition[1] != ("pi"):
+    print("Ułamek dziesiętny")
+    degrees = float(radian_partition[0]) * 180/math.pi
+    print(f"{radian} w mierze kątowej jest równe {degrees}")
+if radian_partition[0].find("/") == True and radian_partition[1] == ("pi"):
+    print("Ułamek zwykły")
+    fraction = radian_partition[0].partition("/")
+    division = float(fraction[0]) / float(fraction[2])
+    degrees = division * 180
+    print(f"{radian} w mierze kątowej jest równe {degrees}")
+if radian_partition[0].find("/") == True and radian_partition[1] != ("pi"):
+    print("Ułamek zwykły")
+    fraction = radian_partition[0].partition("/")
+    division = float(fraction[0]) / float(fraction[2])
+    degrees = division * 180/math.pi
+    print(f"{radian} w mierze kątowej jest równe {degrees}")
