22be13d6df86c5af4ebf5167bfe9818d5315195f
bartekglowacz <bartekglowacz@gmail.com>
2023-01-28 18:55:00 +0100

    Program work as it should. Added result in string format in right order. In my opinion program is finished.

--- decimal_binary.py
+++ decimal_binary.py
@@ -8,23 +8,23 @@ import math
 
 print("Podaj liczbę w systemie dziesiętnym, którą chcesz przekonwertować na system binarny: ")
 decimal_input = int(input())
+decimal_input_global = decimal_input
 
 whole = math.floor(decimal_input / 2)
 rest = decimal_input % 2
 result = []
 
-print(f"Liczba {decimal_input} podzielona przez 2 to {whole} i {rest} reszty")
-# whole ma być nowym decimal input
-
 while decimal_input > 0:
     whole = math.floor(decimal_input / 2)
     rest = decimal_input % 2
     if rest == 1:
-        print("".join("1"), end="")
         result.append("1")
         decimal_input = whole
     if rest == 0:
-        print("".join("0"), end="")
         result.append("0")
         decimal_input = whole
-print(f"Result list: {result}")
+
+result2 = result[::-1]
+# print(f"\nResult list: {result2}")
+string_result = "".join(result2)
+print(f"Decimal form: {decimal_input_global}\nBinary form: {string_result}")
