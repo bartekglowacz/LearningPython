e2e5111034204844b7f860d3cd5ada44d2502adf
bartekglowacz <bartekglowacz@gmail.com>
2023-01-28 22:29:05 +0100

    temp program

---
+++ time_comparison.py
@@ -0,0 +1,6 @@
+import time
+import Hide_card_number
+import Hide_card_number_withoutSpace
+
+print(f"Czas wykonania Hide_card_number = {(Hide_card_number.end_time - Hide_card_number.start_time)}")
+print(f"Czas wykonania Hide_card_number_withoutSpace = {Hide_card_number_withoutSpace.end_time - Hide_card_number_withoutSpace.start_time}")
