4ad7b48d86c2bf7371340671b2ad5d4e0f484141
bartekglowacz <bartekglowacz@gmail.com>
2023-02-01 19:18:15 +0100

    beginning

---
+++ Apply_Discount.py
@@ -0,0 +1,9 @@
+"""
+Apply Discount
+In python, create a function that accepts two parameters.
+The first one must be the full price of an item as an integer and the second one must be the discount percentage as an integer.
+The function must return the price of an item after the discount has been applied.
+For an instance, if the price is 100 and the discount consists of 20, the function must return 80.
+"""
+
+print("scratch")
