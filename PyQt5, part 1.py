f2459e4c8a59a03dd907019adfa5cfa564bd77b7
bartekglowacz <bartekglowacz@gmail.com>
2023-06-03 14:26:46 +0200

    .

---
+++ PyQt5, part 1.py
@@ -0,0 +1,41 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Moje GUI")
+        self.setWindowIcon(QIcon("ico"))
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("My first label")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me!")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you pressed the button!")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+
+window()
